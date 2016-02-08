69ab39e4a5a8e5b6d1fe40bd9d7415c074e1e688
Kyle <Kyle@PinkyThePedophile>
2016-02-07 20:08:36 -0500

    Added preamble.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,10 +4,15 @@ public class HelloWorld {
 	public void greetUser(){
 		System.out.println("Hello World.  I am waiting.");
 	}
-
+	
+	public void givePreamble(){
+		System.out.println("Hear ye hear ye: important announcement.");
+	}
+	
 	public static void main(String[] args) {
 		HelloWorld greetings = new HelloWorld();
-		greetings.greetUser();
+		greetings.givePreamble();
+		greetings.greetUser();	
 
 	}
 
